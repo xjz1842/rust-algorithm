be067539bd82a6fe39234872c5aa7ce6646d27b7
Your Name <your.email@example.com>
2025-01-24 19:56:55 +0800

    optimise

--- src/group_anagrams.rs
+++ src/group_anagrams.rs
@@ -1,27 +1,5 @@
 use std::collections::HashMap;
 
-/**
-use std::collections::HashMap;
-
-impl Solution {
-    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut map: HashMap< String, Vec<String> > = HashMap::new();
-        for s in strs {
-            let mut cv : Vec<char> = s.chars().collect();
-            cv.sort();
-            let os = cv.iter().collect::<String>();
-            let vec: Vec<String> = Vec::new();
-            let val = map.entry(os).or_insert(vec);
-            val.push(s);
-        }
-        let mut res = Vec::new();
-        for val in map.values() {
-            res.push(val.to_owned());
-        }
-        res
-    }
-}
- */
 
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
         let mut result : Vec<Vec<String>> = vec![];
