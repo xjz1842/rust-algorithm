782016e9a8e844b29a5c957b45bef168353c1b03
Your Name <your.email@example.com>
2025-02-04 12:35:35 +0800

    234 回文链表

--- src/linklist/is_palindrome.rs
+++ src/linklist/is_palindrome.rs
@@ -15,18 +15,41 @@ impl ListNode {
   }
 }
 
-pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
-    let mut copy_head: Option<Box<ListNode>> = head;
-    let mut list = vec![];
-    while let Some(node) = copy_head {
-        list.push(node.val);
-        copy_head = node.next;
+// 876. 链表的中间结点
+fn middle_node(head: &Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut fast: &Option<Box<ListNode>> = head;
+    let mut slow = head;
+    while fast.is_some() && fast.as_ref()?.next.is_some() {
+        slow = &slow.as_ref()?.next;
+        fast = &fast.as_ref()?.next.as_ref()?.next;
     }
-     let len = list.len();
-     for i in 0..len/2 {
-        if list[i] != list[len - i - 1] {
-            return false;
+    #[allow(mutable_transmutes)]
+    let slow: &mut Option<Box<ListNode>> = unsafe { std::mem::transmute(slow) };
+    slow.take()
+}   
+
+fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut pre = None;
+        let mut cur = head;
+        while let Some(mut node) = cur {
+            let nxt = node.next;
+            node.next = pre;
+            pre = Some(node);
+            cur = nxt;
         }
-     }
-     true
+        pre
+    }
+
+pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+    let mut head= head;
+    let mid = middle_node(&head);
+        let mut head2 = reverse_list(mid);
+        while head.is_some() {
+            if head.as_ref().unwrap().val != head2.as_ref().unwrap().val {
+                return false;
+            }
+            head = head.unwrap().next;
+            head2 = head2.unwrap().next;
+        }
+        true
 }
